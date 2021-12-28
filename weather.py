a1646a56ef2f02dc5fd00c13c468b8eee5662aac
bryanbarcos <bryanbarcos@hotmail.ca>
2021-12-28 13:50:55 -0500

    Added baseurl field and other fields for user input processing

--- weather.py
+++ weather.py
@@ -1,6 +1,28 @@
 import tkinter as tk
 
 fields = ('API KEY', 'From [YYYY-MM-DD]', 'To [YYYY-MM-DD]', 'Cities')
+baseUrl = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/'
+
+cities_list = []
+from_date = ''
+to_date = ''
+api_key = ''
+
+def process_entries(entries):
+    # period rate:
+    print('FINAL_BALANCE_FUNCTION_CALL')
+    print(entries['From [YYYY-MM-DD]'].get())
+    print(entries['To [YYYY-MM-DD]'].get())
+    print(entries['Cities'].get())
+
+    # Remove whitespaces in case for mistake during user input
+    locations = entries['Cities'].get()
+    locations2 = locations.replace(' ', '')
+    cities_list = locations2.split('-')
+
+    from_date = entries['From [YYYY-MM-DD]'].get()
+    to_date = entries['To [YYYY-MM-DD]'].get()
+    api_key = entries['API KEY'].get()
 
 if __name__ == '__main__':
     import weather_gui as wg
@@ -8,7 +30,7 @@ if __name__ == '__main__':
     root = tk.Tk()
     ents = wg.makeform(root, fields)
     b1 = tk.Button(root, text='Get Average Temp',
-           command=(lambda e=ents: wg.final_balance(e)))
+           command=(lambda e=ents: process_entries(e)))
     b1.pack(side=tk.LEFT, padx=5, pady=5)
     b3 = tk.Button(root, text='Quit', command=root.quit)
     b3.pack(side=tk.LEFT, padx=5, pady=5)
