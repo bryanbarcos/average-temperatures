f6a4edd5906797200755c138e874b4c9ba8bc5fa
bryanbarcos <bryanbarcos@hotmail.ca>
2021-12-29 21:55:57 -0500

    Changed from CSV to JSON response object

--- weather.py
+++ weather.py
@@ -1,11 +1,15 @@
 import tkinter as tk
 import csv
 import codecs
+from urllib import response
+import urllib.request
+import urllib.error
+import sys
+import json
 
 fields = ('API KEY', 'From [YYYY-MM-DD]', 'To [YYYY-MM-DD]', 'Cities', 'Calculated Average Temperature')
 baseUrl = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'
 unitGroup = 'metric'
-contentType = 'csv'
 
 cities_list = []
 from_date = ''
@@ -49,42 +53,27 @@ def process_entries(entries):
     to_date = entries['To [YYYY-MM-DD]'].get()
     api_key = entries['API KEY'].get()
 
-def read_csv(csv_bytes):
-    csv_text = csv.reader(codecs.iterdecode(csv_bytes, 'utf-8'))
-
-    row_index = 0
-
-    for row in csv_text:
-        if row_index == 0:
-            first_row = row
-
-        else:
-            print('Weather in ', row[0], ' on ', row[1])
-
-            col_index = 0
-            for col in row:
-                if col_index >= 4:
-                    print('  ', first_row[col_index], ' = ', row[col_index])
-                col_index += 1
-        row_index += 1
-
-    # If there are no CSV rows then something fundamental went wrong
-    if row_index == 0:
-        print('Sorry, but it appears that there was an error connecting to the weather server.')
-        print('Please check your network connection and try again..')
-
-    # If there is only one CSV  row then we likely got an error from the server
-    if row_index == 1:
-        print('Sorry, but it appears that there was an error retrieving the weather data.')
-        print('Error: ', first_row)
-
 def calculate_average(entries):
     process_entries(entries)
 
     for loc in cities_list:
         api_call = make_url(loc)
-        print(api_call)
-    
+        
+        try:
+            csv_bytes = urllib.request.urlopen(api_call)
+        except urllib.error.HTTPError as e:
+            error_info = e.read().decode()
+            print('Error code: ', e.code, error_info)
+            sys.exit()
+
+        except urllib.error.URLError as e:
+            error_info = e.read().decode()
+            print('Error code: ', e.code, error_info)
+            sys.exit()
+
+        string = csv_bytes.read().decode('utf-8')
+        something = json.loads(string)
+        print(something['days']) 
 
 if __name__ == '__main__':
     import weather_gui as wg
